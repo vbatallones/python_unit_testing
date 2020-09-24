f302635dec2155fdd567c07efc0722e4d5048b3e
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-09-24 12:07:42 -0700

    [calc] Add simple functions for a calculator

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(x, y):
+    """this is add function"""
+    return x + y
+
+def subtract(x, y):
+    """this is subtract function"""
+    return x - y
+
+def multiply(x, y):
+    """this is multiply function"""
+    return x * y
+
+def divide(x, y):
+    """this is divide function"""
+    return x / y
